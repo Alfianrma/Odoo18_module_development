5b2fadb58f0b42495f665018d3e4313664b3f2c1
alfianrma77 <alfianrma77@gmail.com>
2025-07-24 21:23:20 +0700

    add inline views property type

--- addons/estate/models/estate_property_type.py
+++ addons/estate/models/estate_property_type.py
@@ -5,6 +5,11 @@ class EstatePropertyType(models.Model):
     _description = 'Estate Property Type'
 
     name = fields.Char(required=True)
+    property_ids = fields.One2many(
+        comodel_name='estate.property',
+        inverse_name='property_type_id',
+        string='Properties'
+    )
     _sql_constraints = [
         ('name_unique', 'UNIQUE(name)', 'The property type name must be unique.')
     ]
